45d8561992a4e01b13d08ab67f34a1e18339d914
Eric Tsang <surplus.et@gmail.com>
2017-10-23 00:07:42 -0700

    v35.2.10: added default value to static funs
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- lib.simplepipestream/src/main/kotlin/com/github/ericytsang/lib/simplepipestream/StaticFuns.kt
+++ lib.simplepipestream/src/main/kotlin/com/github/ericytsang/lib/simplepipestream/StaticFuns.kt
@@ -2,7 +2,7 @@ package com.github.ericytsang.lib.simplepipestream
 
 object StaticFuns
 {
-    fun newSimplePipeStreamPair(bufferSize:Int?):Pair<SimplePipedInputStream,SimplePipedOutputStream>
+    fun newSimplePipeStreamPair(bufferSize:Int? = null):Pair<SimplePipedInputStream,SimplePipedOutputStream>
     {
         val pipeO = bufferSize?.let {SimplePipedOutputStream(it)} ?: SimplePipedOutputStream()
         val pipeI = SimplePipedInputStream(pipeO)
