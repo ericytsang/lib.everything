aaee1e76f3b6e3326faef2e62d9d07c08fba50b7
Eric Tsang <surplus.et@gmail.com>
2017-10-22 23:14:45 -0700

    added auto-incrementing tagging and pushing gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
+import org.gradle.internal.impldep.org.bouncycastle.util.Properties
 import org.gradle.jvm.tasks.Jar
+import java.util.Properties as JavaProperties
 
 plugins {
     `kotlin-dsl`
 }
 
-subprojects {
+allprojects {
 
     group = "com.github.ericytsang"
-    version = "35.2.0"
+    version = "35.2."+properties["artifact_version"]!!
 
     repositories {
         jcenter()
@@ -50,5 +52,34 @@ subprojects {
 
     artifacts {
         add("archives",sourcesJarTask)
+    }
+}
+
+val installCommitAllAndPushTask = task("install_add_all_commit_and_increment_version_number")
+{
+    dependsOn.addAll(tasks
+        .filter {name != it.name}
+        .filter {"install" in it.name})
+    actions.apply {} += Action<Task> {
+        Runtime.getRuntime().exec("git add --all").waitFor()
+        Runtime.getRuntime().exec("git commit -s -m \"v$version\"").waitFor()
+        Runtime.getRuntime().exec("git push origin master").waitFor()
+
+        val properties = JavaProperties()
+        val propsFile = File("gradle.properties")
+        properties.load(propsFile.inputStream())
+        val currVresionNumber = properties["artifact_version"].toString().toLong()
+        val nextVersionNumber = currVresionNumber.plus(1)
+        properties.setProperty("artifact_version",nextVersionNumber.toString())
+        properties.store(propsFile.outputStream(),null)
+    }
+}
+
+task("tag_and_push")
+{
+    dependsOn.add(installCommitAllAndPushTask)
+    actions.apply {} += Action<Task> {
+        Runtime.getRuntime().exec("git tag -a \"$version\" -m \"release v$version\"").waitFor()
+        Runtime.getRuntime().exec("git push origin $version").waitFor()
     }
 }
