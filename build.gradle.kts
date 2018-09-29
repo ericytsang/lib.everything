1d2853139234549815f76b7d7a5eb0a0ba48ba81
Eric Tsang <surplus.et@gmail.com>
2018-09-29 13:01:48 -0700

    trying to make the gradle scripts work....

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,120 +1,65 @@
-import org.apache.tools.ant.taskdefs.Java
-import org.gradle.internal.impldep.org.bouncycastle.util.Properties
-import org.gradle.jvm.tasks.Jar
-import java.io.InputStream
-import java.util.Properties as JavaProperties
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-plugins {
-    `kotlin-dsl`
-}
+buildscript {
 
-val projectVersion = "36.0.2"
+    repositories {
+        jcenter()
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
 
-subprojects {
-
-    val kotlinVersion = "1.2.40"
-
-    group = "com.github.ericytsang"
-    version = projectVersion
-
-    plugins.apply("maven")
-    plugins.apply("kotlin")
-
-    tasks.withType(JavaCompile::class.java) {
-        sourceCompatibility = JavaVersion.VERSION_1_6.toString()
-        targetCompatibility = JavaVersion.VERSION_1_6.toString()
+    ext {
+        lib_everything_version = '36.0.2'
+        kotlin_version = '1.2.61'
+        room_version = '1.1.1'
+        paging_version = "1.0.1"
+        work_manager_version = '1.0.0-alpha05'
+        android_support_version = '27.1.1'
+        firebase_version = '12.0.1'
+        anko_version = '0.10.4'
     }
 
     dependencies {
-        compile(kotlin("stdlib",kotlinVersion))
-        compile(kotlin("reflect",kotlinVersion))
-        if (name != "lib.testutils")
-        {
-            testCompile(project(":lib.testutils"))
-        }
+        classpath 'com.android.tools.build:gradle:3.1.4'
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
+}
+
+subprojects {
+
+    apply plugin: "maven"
+    apply plugin: "kotlin"
+
+    tasks.withType(JavaCompile) {
+        sourceCompatibility = JavaVersion.VERSION_1_6
+        targetCompatibility = JavaVersion.VERSION_1_6
     }
 
     repositories {
         jcenter()
+        google()
         mavenCentral()
+        maven { url 'https://maven.fabric.io/public' }
+        maven { url 'https://maven.google.com/' }
+        mavenLocal()
     }
 
-    val sourcesJarTask = task("sourcesJar",Jar::class) {
-        dependsOn(tasks.find {it.name == "classes"}!!)
-        classifier = "sources"
-        from(java.sourceSets["main"].allSource)
+    dependencies {
+        compile "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
+        compile "org.jetbrains.kotlin:kotlin-reflect:$kotlin_version"
+
+        testCompile 'junit:junit:4.12'
+        testCompile "org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version"
     }
 
-    artifacts {
-        add("archives",sourcesJarTask)
-    }
-}
-
-task("install_tag_and_push")
-{
-    // install all modules
-    dependsOn.addAll(allprojects
-        .iterator().asSequence()
-        .flatMap {it.tasks.toList().asSequence()}
-        .filter {it.name == "install"})
-
-    actions.apply {} += Action<Task> {
-
-        // make sure working branch is clean
-        executeCommand("git status",{"nothing to commit, working tree clean" in it},{"working branch not clean"})
-
-        // make sure there is no conflicting release
-        executeCommand("git fetch",0)
-        executeCommand("git tag -l",{projectVersion !in it.split("\n")},{"a tag with the name \"$projectVersion\" already exists; please update version number:\n$it"})
-
-        // add tag and push
-        executeCommand("git tag -a \"$projectVersion\" -m \"v$projectVersion\"",0)
-        executeCommand("git push origin $projectVersion",0)
+    if (name != "lib.testutils") {
+        dependencies {
+            testCompile project(":lib.testutils")
+        }
     }
 }
 
-fun executeCommand(
-        command:String,
-        expectedReturnValue:Int=0,
-        failureMessage:(actual:Int)->String={"execution of command \"$command\" returned $it instead of $expectedReturnValue"})
-{
-    // execute process
-    println("$ $command")
-    val process = Runtime.getRuntime().exec(command)
-
-    // read process output
-    process.inputStream.copyTo(System.out)
-    println()
-
-    // await process termination
-    val returnValue = process.waitFor()
-
-    // fail if predicate not satisfied
-    if (returnValue != expectedReturnValue)
-    {
-        throw Exception(failureMessage(returnValue))
-    }
-}
-
-fun executeCommand(
-        command:String,
-        predicate:(output:String)->Boolean,
-        failureMessage:(actualOutput:String)->String={"output of command \"$command\" did not satisfy predicate. raw output:\n$it"})
-{
-    // execute process
-    println("$ $command")
-    val process = Runtime.getRuntime().exec(command)
-
-    // read process output
-    val outputOfProcess = String(process.inputStream.readBytes())
-    println(outputOfProcess)
-
-    // await process termination
-    process.waitFor()
-
-    // fail if predicate not satisfied
-    if (!predicate(outputOfProcess))
-    {
-        throw Exception(failureMessage(outputOfProcess))
-    }
+task clean(type: Delete) {
+    delete rootProject.buildDir
 }
