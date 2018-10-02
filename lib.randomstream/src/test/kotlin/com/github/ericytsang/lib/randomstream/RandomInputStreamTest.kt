73d8c23cd2bd0011cb8816a02b70080afbca1f8a
Eric Tsang <surplus.et@gmail.com>
2018-10-02 16:43:51 -0700

    made tests faster...

--- lib.randomstream/src/test/kotlin/com/github/ericytsang/lib/randomstream/RandomInputStreamTest.kt
+++ lib.randomstream/src/test/kotlin/com/github/ericytsang/lib/randomstream/RandomInputStreamTest.kt
@@ -2,6 +2,8 @@ package com.github.ericytsang.lib.randomstream
 
 import org.junit.Test
 import java.io.DataInputStream
+import java.util.*
+import kotlin.concurrent.thread
 
 class RandomInputStreamTest
 {
@@ -10,14 +12,20 @@ class RandomInputStreamTest
     @Test
     fun generates_all_combinations_of_bits()
     {
-        val allBytes = (Byte.MIN_VALUE..Byte.MAX_VALUE).toMutableSet()
+        val allBytes = Collections.synchronizedSet((Byte.MIN_VALUE..Byte.MAX_VALUE).toMutableSet())
         val ba = byteArrayOf(0)
-        while (allBytes.isNotEmpty())
-        {
-            randomInputStream.read(ba)
-            allBytes.remove(ba[0].toInt())
-            println(allBytes)
-        }
+        (1..4)
+                .map {
+                    thread {
+                        while(allBytes.isNotEmpty())
+                        {
+                            randomInputStream.read(ba)
+                            allBytes.remove(ba[0].toInt())
+                            println(allBytes)
+                        }
+                    }
+                }
+                .forEach {it.join()}
     }
 
     @Test
