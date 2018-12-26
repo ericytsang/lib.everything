723728768e94e2a5c1722e277f92884be1b822e6
Eric Tsang <surplus.et@gmail.com>
2018-12-26 02:08:32 -0800

    new raii api

--- lib.raii/src/main/kotlin/com/github/ericytsang/lib/raii/Raii.kt
+++ lib.raii/src/main/kotlin/com/github/ericytsang/lib/raii/Raii.kt
@@ -1,20 +1,17 @@
 package com.github.ericytsang.lib.raii
 
 import java.io.Closeable
+import java.io.Serializable
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 
-class Raii<Raii:Closeable>:Closeable,ReadOnlyRaii<Raii>
+class Raii<Raii:Closeable>(initialValue:Raii):ReadOnlyRaii<Raii>
 {
-    companion object
-    {
-        class NoopCloseable<Wrapee:Any>(val wrapee:Wrapee):Closeable
-        {
-            override fun close() = Unit
-        }
-    }
-
-    override var obj:Raii? = null
+    override var objOrNullIfClosing:Raii? = initialValue
+        get() = lock.withLock {field}
+        private set
+    override var obj:Raii = initialValue
+        get() = lock.withLock {field}
         private set
 
     private val lock = ReentrantLock()
@@ -22,37 +19,26 @@ class Raii<Raii:Closeable>:Closeable,ReadOnlyRaii<Raii>
 
     private val listeners = mutableSetOf<ReadOnlyRaii.Listener>()
 
-    fun open(raiiInstanceFactory:()->Raii):Raii = lock.withLock()
+    fun set(raiiInstanceFactory:()->Raii):Raii = lock.withLock()
     {
-        close()
+        objOrNullIfClosing = null
+        listeners.forEach {it.onAssigned(ReadOnlyRaii.Listener.Params(this,ReadOnlyRaii.Listener.Action.CLOSING))}
+        obj.close()
         val raiiObject = raiiInstanceFactory()
         obj = raiiObject
-        listeners.forEach {it.onAssigned(this)}
+        objOrNullIfClosing = raiiObject
+        listeners.forEach {it.onAssigned(ReadOnlyRaii.Listener.Params(this,ReadOnlyRaii.Listener.Action.OPENING))}
         unblockWhenObjIsInitialized.signalAll()
         return raiiObject
     }
 
-    override fun close()
-    {
-        getAndClose()
-    }
-
-    fun getAndClose():Raii? = lock.withLock()
-    {
-        val raiiObject = obj
-        obj = null
-        listeners.forEach {it.onAssigned(this)}
-        raiiObject?.close()
-        return raiiObject
-    }
-
-    override fun blockingGetNonNullObj():Raii = lock.withLock()
+    override fun blockingGetNext(old:Raii):Raii = lock.withLock()
     {
         val returnedObj:Raii
         while (true)
         {
             val obj = this.obj
-            if (obj != null)
+            if (obj != old)
             {
                 returnedObj = obj
                 break
@@ -65,48 +51,101 @@ class Raii<Raii:Closeable>:Closeable,ReadOnlyRaii<Raii>
         returnedObj
     }
 
-    override fun <Return:Any> doIfOpen(block:(Raii)->Return):Return? = lock.withLock()
+    override fun <Return:Any> doWithLock(block:(Raii)->Return):Return? = lock.withLock()
     {
-        block(obj?:return@withLock null)
+        block(obj)
+    }
+
+    override fun <Return:Any> doOnNextWithLock(old:Raii,block:(Raii)->Return):Return? = lock.withLock()
+    {
+        block(blockingGetNext(old))
     }
 
     override fun listen(listener:ReadOnlyRaii.Listener):Closeable = lock.withLock()
     {
         listeners += listener
-        listener.onAssigned(this)
+        listener.onAssigned(ReadOnlyRaii.Listener.Params(this,ReadOnlyRaii.Listener.Action.NOTHING))
         Closeable()
         {
             listeners -= listener
+        }
+    }
+
+    class Noop<Wrapee:Any>(val wrapee:Wrapee):Closeable
+    {
+        override fun close() = Unit
+    }
+
+    sealed class Opt<Wrapped:Closeable>:Closeable,Serializable
+    {
+        companion object
+        {
+            fun <Wrapped:Closeable> none():Opt<Wrapped>
+            {
+                return None()
+            }
+            fun <Wrapped:Closeable> some(wrapped:Wrapped):Opt<Wrapped>
+            {
+                return Some(wrapped)
+            }
+        }
+        abstract val opt:Wrapped?
+        data class None<Wrapped:Closeable>(
+                val id:Int = 0)
+            :Opt<Wrapped>()
+        {
+            override val opt:Wrapped? get() = null
+            override fun close() = Unit
+        }
+        data class Some<Wrapped:Closeable>(
+                override val opt:Wrapped)
+            :Opt<Wrapped>()
+        {
+            override fun close() = opt.close()
         }
     }
 }
 
 interface ReadOnlyRaii<Raii:Closeable>
 {
-    val obj:Raii?
-    fun blockingGetNonNullObj():Raii
-    fun <Return:Any> doIfOpen(block:(Raii)->Return):Return?
+    /**
+     * null if closed
+     */
+    val objOrNullIfClosing:Raii?
+    val obj:Raii
+    fun blockingGetNext(old:Raii):Raii
+    fun <Return:Any> doWithLock(block:(Raii)->Return):Return?
+    fun <Return:Any> doOnNextWithLock(old:Raii,block:(Raii)->Return):Return?
     fun listen(listener:Listener):Closeable
 
     interface Listener
     {
-        fun onAssigned(source:Raii<*>)
+        fun onAssigned(params:Params)
+        data class Params(
+                val source:Raii<*>,
+                val action:Action)
+        enum class Action(val isClosing:Boolean)
+        {
+            NOTHING(false),
+            OPENING(false),
+            CLOSING(true);
+        }
     }
 }
 
-fun <Raii:Closeable> ReadOnlyRaii<Raii>.listen(listener:(ReadOnlyRaii<*>)->Unit):Closeable
+fun <Raii:Closeable> ReadOnlyRaii<Raii>.listen(listener:(ReadOnlyRaii.Listener.Params)->Unit):Closeable
 {
     val listenerObject = object:ReadOnlyRaii.Listener
     {
-        override fun onAssigned(source:com.github.ericytsang.lib.raii.Raii<*>)
+        override fun onAssigned(params:ReadOnlyRaii.Listener.Params)
         {
-            listener(source)
+            listener(params)
         }
     }
     return listen(listenerObject)
 }
 
-fun Iterable<ReadOnlyRaii<*>>.listen(listener:(ReadOnlyRaii<*>)->Unit):Closeable
+fun Iterable<ReadOnlyRaii<*>>.listen(listener:(ReadOnlyRaii.Listener.Params)->Unit):Closeable
 {
     val closeables = map {it.listen(listener)}
     return Closeable()
--- lib.raii/src/test/kotlin/com/github/ericytsang/lib/raii/RaiiTest.kt
+++ lib.raii/src/test/kotlin/com/github/ericytsang/lib/raii/RaiiTest.kt
@@ -4,8 +4,6 @@ import com.github.ericytsang.lib.concurrent.awaitSuspended
 import org.junit.Test
 import org.mockito.Mockito.doAnswer
 import org.mockito.Mockito.mock
-import org.mockito.Mockito.never
-import org.mockito.Mockito.verify
 import java.io.Closeable
 import java.util.concurrent.ArrayBlockingQueue
 import java.util.concurrent.LinkedBlockingQueue
@@ -15,99 +13,99 @@ import kotlin.test.assertSame
 
 class RaiiTest
 {
-    private val closeable = mock(Closeable::class.java)
+    private val closeable1 = mock(Closeable::class.java)
+    private val closeable2 = mock(Closeable::class.java)
+
+    @Test
+    fun get_returns_instance_provided_in_constructor()
+    {
+        val raii = Raii<Closeable>(closeable1)
+        assertSame(closeable1,raii.obj)
+    }
 
     @Test
     fun get_returns_instance_provided_in_open()
     {
-        val raii = Raii<Closeable>()
-        raii.open {closeable}
-        assertSame(closeable,raii.obj)
+        val raii = Raii<Closeable>(closeable1)
+        raii.set {closeable2}
+        assertSame(closeable2,raii.obj)
     }
 
     @Test
     fun open_closes_the_previous_object_before_opening_the_new_one()
     {
         val actions = mutableListOf<Int>()
-        val raii = Raii<Closeable>()
-        raii.open {
+        val raii = Raii<Closeable>(closeable1)
+        raii.set {
             actions.add(1)
-            closeable
+            closeable1
         }
-        doAnswer {actions.add(2)}.`when`(closeable).close()
-        raii.open {
+        doAnswer {actions.add(2)}.`when`(closeable1).close()
+        raii.set {
             actions.add(3)
-            closeable
+            closeable1
         }
         assertEquals(listOf(1,2,3),actions)
     }
 
     @Test
-    fun getAndClose_closes_the_object_before_it()
+    fun blockingGetNext_returns_once_raii_is_open()
     {
-        val raii = Raii<Closeable>()
-        raii.open {closeable}
-        verify(closeable,never()).close()
-        assertSame(closeable,raii.getAndClose())
-        verify(closeable).close()
-    }
-
-    @Test
-    fun blockingGetNonNullObj_returns_once_raii_is_open()
-    {
-        val raii = Raii<Closeable>()
+        val raii = Raii<Closeable>(closeable1)
         val q = ArrayBlockingQueue<Closeable>(1)
         val t = thread {
-            q.put(raii.blockingGetNonNullObj())
+            q.put(raii.blockingGetNext(closeable1))
         }
         t.awaitSuspended()
-        raii.open {closeable}
+        raii.set {closeable2}
         t.join()
-        assertSame(closeable,q.take())
+        assertSame(closeable2,q.take())
     }
 
     @Test
-    fun blockingGetNonNullObj_does_not_return_when_closed_then_returns_once_raii_is_open()
+    fun blockingGetNext_does_not_return_when_raii_open_with_same_obj_and_returns_when_raii_is_open_with_different_obj()
     {
-        val raii = Raii<Closeable>()
+        val raii = Raii<Closeable>(closeable1)
         val q = ArrayBlockingQueue<Closeable>(1)
         val t = thread {
-            q.put(raii.blockingGetNonNullObj())
+            q.put(raii.blockingGetNext(closeable1))
         }
         t.awaitSuspended()
-        raii.getAndClose()
-        raii.close()
+        raii.set {closeable1}
         t.awaitSuspended()
         check(t.isAlive)
-        raii.open {closeable}
+        raii.set {closeable2}
         t.join()
-        assertSame(closeable,q.take())
+        assertSame(closeable2,q.take())
     }
 
     @Test
     fun listener_triggered_after_open()
     {
         val q = LinkedBlockingQueue<Int>()
-        val raii = Raii<Closeable>()
+        val raii = Raii(Closeable {q += 0})
+        assertEquals(listOf(),generateSequence {q.poll()}.toList())
+
         raii.listen {q += 2}
-        raii.open {
+        assertEquals(listOf(2),generateSequence {q.poll()}.toList())
+
+        raii.set {
             q += 1
             Closeable {q += 3}
         }
-        assertEquals(
-                listOf(2,2,1,2),
-                generateSequence {q.poll()}.toList())
+        assertEquals(listOf(2,0,1,2),generateSequence {q.poll()}.toList())
     }
 
     @Test
     fun listener_triggered_after_open_and_before_close()
     {
         val q = LinkedBlockingQueue<Int>()
-        val raii = Raii<Closeable>()
+        val oldCloseable = Closeable {q += 0}
+        val raii = Raii(oldCloseable)
         assertEquals(listOf(),generateSequence {q.poll()}.toList())
 
         raii.listen {
-            q += if (raii.obj != null)
+            q += if (raii.obj != oldCloseable)
             {
                 2
             }
@@ -115,63 +113,64 @@ class RaiiTest
             {
                 3
             }
+            q += if (raii.objOrNullIfClosing != null)
+            {
+                4
+            }
+            else
+            {
+                5
+            }
         }
-        assertEquals(generateSequence {q.poll()}.toList(),listOf(3))
+        assertEquals(listOf(3,4),generateSequence {q.poll()}.toList())
 
-        raii.open {
+        raii.set {
             q += 1
-            Closeable {q += 4}
+            Closeable {q += 6}
         }
-        assertEquals(generateSequence {q.poll()}.toList(),listOf(3,1,2))
-
-        raii.close()
-        assertEquals(generateSequence {q.poll()}.toList(),listOf(3,4))
+        assertEquals(listOf(3,5,0,1,2,4),generateSequence {q.poll()}.toList())
     }
 
     @Test
     fun listener_on_multiple_raiis()
     {
         val q = LinkedBlockingQueue<Int>()
-        val raii1 = Raii<Closeable>()
-        val raii2 = Raii<Closeable>()
+        val oldCloseable1 = Closeable {q += 0}
+        val oldCloseable2 = Closeable {q += 1}
+        val raii1 = Raii(oldCloseable1)
+        val raii2 = Raii(oldCloseable2)
         assertEquals(listOf(),generateSequence {q.poll()}.toList())
 
         listOf(raii1,raii2).listen {
-            q += if (raii1.obj != null)
-            {
-                1
-            }
-            else
+            q += if (raii1.obj != oldCloseable1)
             {
                 2
             }
-            q += if (raii2.obj != null)
+            else
             {
                 3
             }
-            else
+            q += if (raii2.obj != oldCloseable2)
             {
                 4
             }
+            else
+            {
+                5
+            }
         }
-        assertEquals(listOf(2,4,2,4),generateSequence {q.poll()}.toList())
+        assertEquals(listOf(3,5,3,5),generateSequence {q.poll()}.toList())
 
-        raii1.open {
-            q += 5
-            Closeable {q += 6}
+        raii1.set {
+            q += 6
+            Closeable {q += 7}
         }
-        assertEquals(listOf(2,4,5,1,4),generateSequence {q.poll()}.toList())
+        assertEquals(listOf(3,5,0,6,2,5),generateSequence {q.poll()}.toList())
 
-        raii2.open {
-            q += 7
-            Closeable {q += 8}
+        raii2.set {
+            q += 8
+            Closeable {q += 9}
         }
-        assertEquals(listOf(1,4,7,1,3),generateSequence {q.poll()}.toList())
-
-        raii1.close()
-        assertEquals(listOf(2,3,6),generateSequence {q.poll()}.toList())
-
-        raii2.close()
-        assertEquals(listOf(2,4,8),generateSequence {q.poll()}.toList())
+        assertEquals(listOf(2,5,1,8,2,4),generateSequence {q.poll()}.toList())
     }
 }
