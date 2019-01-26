b7eaf9604e746cd939c9d762c79e67b541b97f77
Eric Tsang <surplus.et@gmail.com>
2019-01-26 00:44:04 -0800

    fixed unit test

--- lib.getfileandline/src/test/kotlin/com/github/ericytsang/lib/typedstream/Tests.kt
+++ lib.getfileandline/src/test/kotlin/com/github/ericytsang/lib/typedstream/Tests.kt
@@ -1,5 +1,7 @@
 package com.github.ericytsang.lib.typedstream
 
+import com.github.ericytsang.lib.getfileandline.StacktraceIndex
+import com.github.ericytsang.lib.getfileandline.getFileNameAndLine
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -8,6 +10,6 @@ class Tests
     @Test
     fun return_file_and_line_number_of_call_site()
     {
-        assertEquals("${this::class.simpleName}.kt:11",getFileNameAndLine(StacktraceIndex()))
+        assertEquals("${this::class.simpleName}.kt:13",getFileNameAndLine(StacktraceIndex()))
     }
 }
