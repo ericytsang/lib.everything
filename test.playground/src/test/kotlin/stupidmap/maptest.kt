0297c34875272f3ff4f996be86ecc32b7ec6ef86
Eric Tsang <surplus.et@gmail.com>
2019-01-31 23:17:13 -0800

    playing with serializable...

--- test.playground/src/test/kotlin/stupidmap/maptest.kt
+++ test.playground/src/test/kotlin/stupidmap/maptest.kt
@@ -1,6 +1,11 @@
 package stupidmap
 
 import org.junit.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.io.Serializable
 import kotlin.test.assertEquals
 
 class maptest
@@ -31,5 +36,41 @@ class maptest
     {
         val map = mutableMapOf("hi" to "bye")
         assertEquals("bye",map.computeIfAbsent("hi") {"hi"})
+    }
+
+    @Test
+    fun serialize_test()
+    {
+        val a = A("HELLOOooooThereeee")
+        val b = B("adasdasdasd")
+        val byteOs = ByteArrayOutputStream()
+        val objOs = ObjectOutputStream(byteOs)
+        objOs.writeObject(a)
+        objOs.writeObject(b)
+        objOs.writeObject(a)
+        objOs.writeObject(b)
+        val byteIs = ByteArrayInputStream(byteOs.toByteArray())
+        val objIs = ObjectInputStream(byteIs)
+        println(objIs.readObject())
+        println(objIs.readObject())
+        println(objIs.readObject())
+        println(objIs.readObject())
+    }
+
+    data class A(val string:String):Serializable
+    {
+        companion object
+        {
+            @JvmStatic
+            private val serialVersionUID = 62784392L
+        }
+    }
+    data class B(val string:String):Serializable
+    {
+        companion object
+        {
+            @JvmStatic
+            private val serialVersionUID = 62784392L
+        }
     }
 }
