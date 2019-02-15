7c845ea55653c51a05abe762eacbb584cdd9cc96
Eric Tsang <surplus.et@gmail.com>
2019-02-15 15:56:28 -0800

    added extension method for the clipboard manager

--- androidlib.core/src/main/java/com/github/ericytsang/androidlib/core/Extensions.kt
+++ androidlib.core/src/main/java/com/github/ericytsang/androidlib/core/Extensions.kt
@@ -5,6 +5,7 @@ import android.app.AlarmManager
 import android.app.KeyguardManager
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -102,6 +103,11 @@ val Context.accessibilityManager:AccessibilityManager get()
 val Context.windowManager:WindowManager get()
 {
     return getSystemServiceCompat(Context.WINDOW_SERVICE)
+}
+
+val Context.clipboardManager:ClipboardManager get()
+{
+    return getSystemServiceCompat(Context.CLIPBOARD_SERVICE)
 }
 
 inline fun <reified SystemService> Context.getSystemServiceCompat(key:String):SystemService
