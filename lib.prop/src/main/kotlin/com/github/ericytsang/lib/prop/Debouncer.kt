9f68454868e98e9e71a780fc5201205a072ccd78
Eric Tsang <surplus.et@gmail.com>
2019-07-28 18:28:54 -0700

    more operator overloading!

--- lib.prop/src/main/kotlin/com/github/ericytsang/lib/prop/Debouncer.kt
+++ lib.prop/src/main/kotlin/com/github/ericytsang/lib/prop/Debouncer.kt
@@ -4,11 +4,11 @@ class Debouncer<Context:Any,Value:Any>(
         private val decorated:MutableProp<Context,Value>)
     :MutableProp<Context,Value> by decorated
 {
-    override fun set(context:Context,value:Value):Value
+    override operator fun set(context:Context,value:Value):Value
     {
-        if (decorated.get(context) != value)
+        if (decorated[context] != value)
         {
-            decorated.set(context,value)
+            decorated[context] = value
         }
         return value
     }
--- lib.prop/src/main/kotlin/com/github/ericytsang/lib/prop/MutableProp.kt
+++ lib.prop/src/main/kotlin/com/github/ericytsang/lib/prop/MutableProp.kt
@@ -2,5 +2,5 @@ package com.github.ericytsang.lib.prop
 
 interface MutableProp<Context:Any,Value:Any>:ReadOnlyProp<Context,Value>
 {
-    fun set(context:Context,value:Value):Value
+    operator fun set(context:Context,value:Value):Value
 }
