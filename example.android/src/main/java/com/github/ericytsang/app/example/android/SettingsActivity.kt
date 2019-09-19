a033396505ceccc595d34ea699d498f6c9019cc3
Eric Tsang <surplus.et@gmail.com>
2019-09-19 11:56:57 -0700

    pressing on "up" button finishes the settings activity

--- example.android/src/main/java/com/github/ericytsang/app/example/android/SettingsActivity.kt
+++ example.android/src/main/java/com/github/ericytsang/app/example/android/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.github.ericytsang.app.example.android
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import com.github.ericytsang.androidlib.core.activity.ContextCompanionWithStart
@@ -25,6 +26,19 @@ class SettingsActivity:AppCompatActivity()
                 .replace(R.id.settings,SettingsFragment())
                 .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item:MenuItem):Boolean
+    {
+        return when(item.itemId)
+        {
+            android.R.id.home ->
+            {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     class SettingsFragment:PreferenceFragmentCompat()
