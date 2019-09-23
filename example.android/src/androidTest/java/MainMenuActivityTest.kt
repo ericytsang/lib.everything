98036de1fffbd03130b975fd0f64889680c21388
Eric Tsang <surplus.et@gmail.com>
2019-09-22 22:22:33 -0700

    renaming tests

--- example.android/src/androidTest/java/MainMenuActivityTest.kt
+++ example.android/src/androidTest/java/MainMenuActivityTest.kt
@@ -34,42 +34,42 @@ class MainMenuActivityTest
     /* confirm dialog tests */
 
     @Test
-    fun confirm_dialog_opens_on_button_clicked()
+    fun confirmDialog_opens_on_button_clicked()
     {
         onView(withId(R.id.confirm_dialog_button)).perform(click())
         onView(withId(ConfirmDialogR.id.textview)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun confirm_title_displays_title()
+    fun confirmDialog_title_displays_title()
     {
         val titleText = Random.Default.randomString(randomInputStringLength)
         onView(withId(R.id.confirm_title_input)).perform(typeText(titleText))
-        confirm_dialog_opens_on_button_clicked()
+        confirmDialog_opens_on_button_clicked()
         onView(withText(titleText)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun confirm_summary_displays_summary()
+    fun confirmDialog_summary_displays_summary()
     {
         val titleText = Random.Default.randomString(randomInputStringLength)
         onView(withId(R.id.confirm_prompt_input)).perform(typeText(titleText))
-        confirm_dialog_opens_on_button_clicked()
+        confirmDialog_opens_on_button_clicked()
         onView(withId(ConfirmDialogR.id.textview)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun confirm_OnActivityResult_negative_button()
+    fun confirmDialog_OnActivityResult_negative_button()
     {
-        confirm_dialog_opens_on_button_clicked()
+        confirmDialog_opens_on_button_clicked()
         onView(withId(ConfirmDialogR.id.button__cancel)).perform(click())
         onView(withId(R.id.confirm_result_output)).check(matches(withText(ConfirmDialogActivity.ButtonId.NO_BUTTON.name)))
     }
 
     @Test
-    fun confirm_OnActivityResult_positive_button()
+    fun confirmDialog_OnActivityResult_positive_button()
     {
-        confirm_dialog_opens_on_button_clicked()
+        confirmDialog_opens_on_button_clicked()
         onView(withId(ConfirmDialogR.id.button__ok)).perform(click())
         onView(withId(R.id.confirm_result_output)).check(matches(withText(ConfirmDialogActivity.ButtonId.YES_BUTTON.name)))
     }
