a5ec683302d50f7c92ce6757413b0120aba5b869
Eric Tsang <surplus.et@gmail.com>
2019-11-09 15:48:20 -0800

    more coroutine testing

--- test.playground/src/test/kotlin/coroutines/CoroutineTest.kt
+++ test.playground/src/test/kotlin/coroutines/CoroutineTest.kt
@@ -1,16 +1,21 @@
 package coroutines
 
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.setMain
-import org.junit.Ignore
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.yield
 import org.junit.Test
+import java.lang.System.*
+import java.lang.Thread.currentThread
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
@@ -48,6 +53,64 @@ class CoroutineTest
             deferred.await()
             deferred.cancelAndJoin()
             assertEquals(5,deferred.await())
+        }
+    }
+
+    private suspend fun waitForASecond()
+    {
+        val endTime = currentTimeMillis()+1000
+        while (currentTimeMillis() < endTime)
+        {
+            yield()
+        }
+        println("waited for 1 second")
+    }
+
+    private suspend fun networkRequest() = withContext(Dispatchers.IO)
+    {
+        logCurrentThreadName("4")
+        repeat(5) {waitForASecond()}
+        42
+    }
+
+    private fun logCurrentThreadName(label: String)
+    {
+        println("#$label ${currentThread().name}")
+    }
+
+    @Test
+    fun letNetworkRequestFinish()
+    {
+        runBlocking()
+        {
+            logCurrentThreadName("1")
+            launch()
+            {
+                val result = networkRequest()
+                logCurrentThreadName("2")
+                println("i got the answer!: $result")
+            }
+            logCurrentThreadName("3")
+            println("hi, i'm not blocked lol")
+        }
+    }
+
+    @Test
+    fun cancelNetworkRequest()
+    {
+        runBlocking()
+        {
+            logCurrentThreadName("1")
+            val coroutineScope = launch()
+            {
+                val result = networkRequest()
+                logCurrentThreadName("2")
+                println("i got the answer!: $result")
+            }
+            logCurrentThreadName("3")
+            waitForASecond()
+            coroutineScope.cancel()
+            println("hi, i'm not blocked lol")
         }
     }
 }
