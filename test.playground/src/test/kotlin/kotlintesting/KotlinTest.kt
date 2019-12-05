f641b9c04292db919d19da929697833d2f89099e
Eric Tsang <surplus.et@gmail.com>
2019-12-04 23:06:39 -0800

    more class tests
    (cherry picked from commit b3665927440c406c395956e36e09fb10d6aad342)

--- test.playground/src/test/kotlin/kotlintesting/KotlinTest.kt
+++ test.playground/src/test/kotlin/kotlintesting/KotlinTest.kt
@@ -8,4 +8,13 @@ class KotlinTest {
     fun `are classes equal`() {
         assert(1::class == 5::class)
     }
+
+    @Test
+    fun `are classes non equal despite references being the same`() {
+        val a: Number = 1
+        val b: Number = 1.5
+        assert(a::class != b::class)
+        assert(a::class == 7::class)
+        assert(b::class == 0.5::class)
+    }
 }
